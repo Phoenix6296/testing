f1cef805feb7341bc84ecda7be3de63a95fcbe49
Krishna <krishnabiswakarmasc@gmail.com>
2023-01-14 11:52:34 +0530

    Car rent page is made

--- src/Components/AvailableCars.js
+++ src/Components/AvailableCars.js
@@ -1,52 +1,34 @@
-import React from 'react'
+import styles from './AvailableCars.module.css'
 import Card from './Card'
 import Car1 from '../Assets/car1.webp'
 import Car2 from '../Assets/car2.jpeg'
 import Car3 from '../Assets/car3.jpeg'
 import Car4 from '../Assets/car4.webp'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const data = [
-    {
-        key: 1,
-        carImage: Car1,
-        carName: 'Hyndai Grand i10',
-        color: 'White',
-        seats: '4',
-        price: '350',
-    },
-    {
-        key: 2,
-        carImage: Car2,
-        carName: 'Ford EcoSport',
-        color: 'Deep Blue',
-        seats: '4',
-        price: '1350',
-    },
-    {
-        key: 3,
-        carImage: Car3,
-        carName: 'Maruti Suzuki XL6',
-        color: 'Gloss Black',
-        seats: '6',
-        price: '850',
-    },
-    {
-        key: 4,
-        carImage: Car4,
-        carName: 'Honda CR-V',
-        color: 'Blue',
-        seats: '4',
-        price: '450',
-    }
+    { key: 1, carImage: Car1, carName: 'Hyndai Grand i10', color: 'White', seats: '4', price: '350', },
+    { key: 2, carImage: Car2, carName: 'Ford EcoSport', color: 'Deep Blue', seats: '4', price: '1350', },
+    { key: 3, carImage: Car3, carName: 'Maruti Suzuki XL6', color: 'Gloss Black', seats: '6', price: '850', },
+    { key: 4, carImage: Car4, carName: 'Honda CR-V', color: 'Blue', seats: '4', price: '450', }
 ]
 const AvailableCars = () => {
     return (
         <>
-            {data.map((item) => { return (<Card key={item.key} carImage={item.carImage} carName={item.carName} color={item.color} seats={item.seats} price={item.price} />) })}
-            <footer>
-                <h2>Rent Vroom</h2>
-                <p>No: 296, 3rd Cross Rd, Jakkasandra, 1st Block, Koramangla Bengaluru, Karnataka 560034</p>
-            </footer>
+            <div className='container'>
+                <h1 className='my-4'>Cars for Rent</h1>
+                <hr />
+                {data.map((item) => { return (<Card key={item.key} carImage={item.carImage} carName={item.carName} color={item.color} seats={item.seats} price={item.price} />) })}
+                <footer className={`styles.footer my-3`}>
+                    <h2>Rent Vroom</h2>
+                    <p>No: 296, 3rd Cross Rd, Jakkasandra, 1st Block, Koramangla Bengaluru, Karnataka 560034</p>
+                    <div className={`${styles.icons} center d-flex justify-content-around`}>
+                        <i className="bi bi-facebook"></i>
+                        <i className="bi bi-instagram"></i>
+                        <i className="bi bi-twitter"></i>
+                    </div>
+                </footer>
+            </div>
         </>
     )
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,29 +1,30 @@
 import React from 'react'
 import styles from './Card.module.css'
 import "bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Card = (props) => {
     return (
-        <div className={`${styles.card__container} ${styles.center}`}>
+        <div className={`${styles.card__container} ${styles.center} mb-3`}>
             <div className={`${styles.card__image_text}`}>
-                <img src={props.carImage} alt="Car1" />
-                <div className={`${styles.card__text} ${styles.center} `}>
+                <img src={props.carImage} alt="Car" />
+                <div className={`${styles.card__text} center d-flex`}>
                     <h4>{props.carName}</h4>
                     <div>
-                        <div className={`${styles.card__icons} ${styles.center}`}>
-                            <i class="bi bi-eyedropper"></i>
+                        <div className={`${styles.card__icons} center d-flex`}>
+                            <i className="bi bi-eyedropper"></i>
                             <p>{props.color}</p>
                         </div>
-                        <div className={`${styles.card__icons} ${styles.center}`}>
-                            <i class="bi bi-car-front"></i>
+                        <div className={`${styles.card__icons} center d-flex`}>
+                            <i className="bi bi-car-front"></i>
                             <p>{props.seats}Seater</p>
                         </div>
                     </div>
                 </div>
             </div>
             <p>₹{props.price}</p>
-            <button className={styles.card__button}>Book Now</button>
-            <button className={styles.card__button}>Details</button>
+            <button type="button" class="btn btn-secondary btn-medium">Book Now</button>
+            <button type="button" class="btn btn-secondary btn-medium">Details</button>
         </div>
     )
 }
